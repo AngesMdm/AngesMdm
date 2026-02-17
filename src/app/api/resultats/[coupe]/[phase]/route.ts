23f36e00fcf8fc478ef5515b2f19692fd0b2ceb7
Graziani Leo <leo.graziani@etu.u-bordeaux.fr>
2026-02-17 22:21:30 +0100

    résultats

--- src/app/api/resultats/[coupe]/[phase]/route.ts
+++ src/app/api/resultats/[coupe]/[phase]/route.ts
@@ -13,10 +13,9 @@ export async function GET(req: Request, context: any): Promise<Response> {
         return new Response(JSON.stringify({ error: "Phase non spécifiée" }), { status: 400 });
     }
     try {
-        const isDevelopment = process.env.NODE_ENV === 'development' && process.env.MODE !== 'production';
-
+        const baseUrl = process.env.BLOB_RESULTS_URL;
         const res = await fetch(
-            `${isDevelopment ? process.env.BLOB_RESULTS_URL : process.env.BLOB_READ_WRITE_TOKEN}${encodeURIComponent(decodedCoupe)}/${encodeURIComponent(decodedPhase)}/resultats.json`,
+            `${baseUrl}${encodeURIComponent(decodedCoupe)}/${encodeURIComponent(decodedPhase)}/resultats.json`,
             { cache: "no-store" }
         );
         if (!res.ok) {
